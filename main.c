0b055fb5eefad86eff6efd024e4b3f8460852cc5
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-09-25 15:33:52 +0900

    실습02

--- main.c
+++ main.c
@@ -3,19 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-  int x, y, z, m;
-  int a, b, c;
+  int x, y;
   
-  x = 2;
-  z = 1;
-  a = 3;
-  b = 4;
-  c = 5;
-  
-  y = a*(x*x)+b*x+c;
-  m = (x+y+z)/3;
-  
-  printf("y=%d, m=%d\n", y, m);
+  printf("Input two integers : ");
+  scanf("%i %i", &x, &y);
+
+  printf("+ result is %d\n", x+y);
+  printf("- result is %d\n", x-y);
+  printf("* result is %d\n", x*y);
+  printf("/ result is %d\n", x/y);
+  printf("%% result is %d\n", x%y);
   system("PAUSE");
   return 0;
 }
